a4e28346fb88b856b66dac7ef0cc63c575de2523
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2023-09-28 14:30:20 +0300

    create calc(switchcase)

---
+++ calc(switchcase).cpp
@@ -0,0 +1,51 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num, num1;
+    char operation;
+    cin >> num>>num1;
+    cin >> operation;
+    switch (operation) {
+    case '+':
+        cout << num + num1;
+        break;
+    case '-':
+        cout << num - num1;
+        break;
+    case '*':
+        cout << num * num1;
+        break;
+    case '/':
+        cout << num / num1;
+        break;
+    default:
+        cout << "tak nizya";
+
+
+    }
+    if (num == 0 )
+        cout << "zero nolzya";
+    else
+        cout << " vi maladec";
+
+    
+
+        
+        
+
+}
+    
+    
+
+
+
+
+
+
+
+
+
+
