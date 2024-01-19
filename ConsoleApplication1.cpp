61fb216f47ce31b0a3a3bb9f7c8922c0b613a609
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-01-19 16:01:01 +0300

    Functions with arrays

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,73 +1,96 @@
 ﻿#include <iostream>
 
+
 using namespace std;
 
-void rectangle(int N, int K) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K ; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
 
-}
-
-void faktorial(int a, int sum) {
-    for (int i = 1; i <= a; i++) {
-        sum *= i;
-        cout << sum << endl;
-    }
-}
-
-bool simpleNumber(int a ) {
-    for (int i = 2; i <= sqrt(a); i++) {
-        if (a % i == 0) {
-            return false;
-        }
-        else
-            return true;
-
+void showArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
     }
     cout << endl;
-
 }
-
-void qubeNumber(int a) {
-    cout << pow(a, 3);
-}
-
-float maxNumber(float a, float b) {
-    if (a > b) return a;
-    else return  b;
-}
-
-bool identifyNum(int a) {
-    if (a > 0) return true;
-    else return false;
-}
-
-
-
-
-
-////000
-
-
-void sepeni(float a, float b, float c) {
-    cout << pow(c, (a / b));
-}
-
-
-
-void summa(int min, int max, int sum) {
-    if (min > max) swap(min, max);
-    for (int i = min; i <= max; i++) {
-        sum += i;
+int maxElement( int array[], int size) {
+    int max = array[0];
+    for (int i = 0; i < size; i++) {
+        if (max < array[i]) max = array[i];
     }
-    cout << sum;
+    return max;
 }
+int minElement( int array[], int size) {
+    int min = array[0];
+    
+    for (int i = 0; i < size; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    
+    return min;
+}
+int minIndexElement(int array[], int size) {
+    int min = array[0];
+    int minIndex = 0;
+    for (int i = 0; i < size; i++) {
+        if (min > array[i]) {
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+int maxIndexElement(int array[], int size) {
+    int max = array[0];
+    int maxIndex = 0;
+    for (int i = 0; i < size; i++) {
+        if (max < array[i]) {
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+int summaElement(int array[], int size) {
+    int summa = 0;
+    for (int i = 0; i < size; i++) {
+        summa += array[i];
+    }
+    return summa;
+}
+float avgElement(int array[], int size) {
+    float avg = 0;
+    for (int i = 0; i < size; i++) {
+        avg += array[i];
+    }
+    avg /= size;
+    return avg;
+}
+int massArray(int array[], int size, int array2[], int size2) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    }
+    for (int i = 0; i < size2; i++) {
+        sum += array2[i];
+    }
+    return sum;
 
 
+}
+void swapMass(int array[], int size) {
+    for (int i=0; i < size / 2; i++) {
+        swap(array[i], array[size - 1-i]);
+        
+        }
+   
+    
+}
+void lolSwapMass(int array[], int array2[], int array3[], int size,int size2, int size3) {
+    for (int i = 0; i < size2; i++) {
+       
+        swap(array[array2[i]], array[array3[i]]);
+    }
+}
 
 
 
@@ -75,18 +98,30 @@ void summa(int min, int max, int sum) {
 
 int main()
 {
-  
-    cout << "show Rectangle " << endl;
-    rectangle(2, 5);
-    cout << "factorial 5 " << endl;
-     faktorial(5, 1);
-    cout << "simple Number 121  " ;
-    cout << simpleNumber(121);
+    int array2[] = { 5,1,3,6 };
+    int array[] = { 3,3,5,73,1,6,9,3,89 };
+    int array3[] = { 1,2,3,5 };
+    int size = sizeof(array) / sizeof(array[0]);
+    int size2 = sizeof(array2) / sizeof(array2[0]);
+    int size3 = sizeof(array3) / sizeof(array3[0]);
+    showArray(array, size);
     cout << endl;
-    cout << "max Number 7,9  ";
-    cout << maxNumber(7, 9);
+    cout << maxElement(array, size) << " is max element";
     cout << endl;
-    cout << "identify Number  " ;
-    cout << identifyNum(7);
+    cout << minElement(array, size) << " is  min element";
     cout << endl;
+    cout << maxIndexElement(array, size) << " is index max element";
+    cout << endl;
+    cout << minIndexElement(array, size) << " is index min element";
+    cout << endl;
+    cout << summaElement(array, size) << " is summa element";
+    cout << endl;
+    cout << avgElement(array, size) << " is  average element";
+    cout << endl;
+    cout << massArray(array, size, array2,size2) << " is  summa elementov";
+    swapMass(array, size);
+    showArray(array, size);
+    lolSwapMass(array, array2, array3, size, size2, size3);
+
 }
+
