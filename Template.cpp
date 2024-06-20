ff101a52bb0aab51137741bbe5844f5da8e58660
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-06-20 11:01:58 +0300

    Template pattern
    Realisation Template pattern

---
+++ Template.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Coffee {
+public:
+	
+	void prepareRecipe() {
+		boilWater();
+		brewCoffee();
+		pourInCup();
+		addCondiments();
+
+	}
+
+protected:
+	void boilWater() {
+		cout << "Boiling Water" << endl;
+	}
+
+	void pourInCup() {
+		cout << "Pour into cup" << endl;
+	}
+
+	virtual void brewCoffee()=0;
+
+	virtual void addCondiments() = 0;
+
+};
+
+
+class BlackCoffee : public Coffee{
+protected:
+	void brewCoffee() override {
+		cout << "Driping coffee through filter" << endl;
+	}
+
+	void addCondiments() override {
+		cout << "No condiments" << endl;
+	}
+
+};
+
+
+class Latte : public Coffee {
+protected:
+	void brewCoffee() override {
+		cout << "Making espresso " << endl;
+	}
+
+	void addCondiments() override {
+		cout << "Adding steamed milk" << endl;
+	}
+
+};
+
+
+
+int main()
+{
+	Coffee* blackCoffee = new BlackCoffee();
+
+	Coffee* latte = new Latte();
+
+
+
+	latte->prepareRecipe();
+}
+
