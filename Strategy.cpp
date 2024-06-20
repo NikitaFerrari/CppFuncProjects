a1ebd7f38e70422764ea60a767ab0991bc9e5cb7
GENJUTSU <145016501+NikitaFerrari@users.noreply.github.com>
2024-06-20 11:01:28 +0300

    Strategy pattern
    Realisation Strategy pattern

---
+++ Strategy.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class ShippingCostStrategy {
+public:
+	virtual ~ShippingCostStrategy() = default;
+	
+	virtual double calculateCost(double weight) = 0;
+
+};
+
+class GroundShipping : public ShippingCostStrategy {
+public:
+	double calculateCost(double weight) override{
+		return weight * 1.5;
+	}
+};
+
+
+class AeroShipping : public ShippingCostStrategy {
+public:
+	double calculateCost(double weight) override {
+		return weight * 3;
+	}
+};
+
+
+class WaterShipping : public ShippingCostStrategy {
+public:
+	double calculateCost(double weight) override {
+		return weight * 2.2;
+	}
+};
+
+
+class Order {
+private:
+	double weight;
+	ShippingCostStrategy* strategy;
+public:
+	Order(double weight, ShippingCostStrategy* strategy) : weight(weight), strategy(strategy){}
+
+	void setShippingStrategy(ShippingCostStrategy* strategy) {
+		this->strategy = strategy;
+	}
+
+	double calculateShippingCost() {
+		return strategy->calculateCost(this->weight);
+	}
+
+
+};
+
+
+
+
+
+
+int main()
+{
+	GroundShipping ground;
+	AeroShipping air;
+	WaterShipping water;
+	Order order(10.0, &ground);
+	cout<<order.calculateShippingCost();
+	order.setShippingStrategy(&air);
+	cout <<order.calculateShippingCost();
+	order.setShippingStrategy(&water);
+	cout << order.calculateShippingCost();
+}
